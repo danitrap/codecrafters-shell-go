587673e27e319bd2ab008ea46abb24e546a5d504
Daniele Trapani <daniele.trapani@outlook.it>
2024-12-29 13:31:06 +0100

    add main loop

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,21 +7,19 @@ import (
 	"strings"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Fprint
-
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Println("Error reading input: ", err)
-		return
+		// Wait for user input
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading input: ", err)
+			return
+		}
+
+		parts := strings.Split(strings.TrimSpace(input), " ")
+
+		fmt.Println(parts[0] + ": command not found")
 	}
-
-	parts := strings.Split(strings.TrimSpace(input), " ")
-
-	fmt.Println(parts[0] + ": command not found")
 }
